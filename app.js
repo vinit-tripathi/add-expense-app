c2f755d4073797407d51bb3edabba7776db43f39
vinit-tripathi <74803056+vinit-tripathi@users.noreply.github.com>
2021-01-14 07:52:26 +0530

    feat: list added

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
-const outputValue = document.querySelector("#textOutput");
-    
-            let totalExpense = 0;
-    
-            outputValue.textContent = totalExpense;
-            // onButtonClick add Input Amount to totalExpense
-            function addExpenseToTotal(){
-                // read value from input
-                const inputElement = document.querySelector("#inputAmount");
-                const textAmount = inputElement.value;
-                
-    
-                // convert it to number
-                const expense = parseInt(textAmount, 10);
-                
-    
-                // add that value to total expense
-                totalExpense = totalExpense + expense;
-                
-                // set output to total expense
-                outputValue.textContent = totalExpense;
-    
-            }
-            
-            
-    
-            
-            const element = document.querySelector("#btnAddExpense");
-            element.addEventListener("click", addExpenseToTotal, false);
-    
+// get the heading element
+const headingEl = document.querySelector("#headingTotal");
+
+// get the reference to desc element
+const inputDescEl = document.querySelector("#inputDesc");
+
+// ref to input amount
+const inputElement = document.querySelector("#inputAmount");
+
+// initial value of expense at 0
+let totalExpense = 0;
+
+// all expenses at one place
+const allExpenses = [];
+
+// onButtonClick add Input Amount to totalExpense
+function addExpenseToTotal() {
+
+    const expenseItem = {};
+
+    // read value from input
+    const textAmount = inputElement.value;
+
+    // read the input from input Desc
+    const textDesc = inputDescEl.value;
+
+    // convert it to number
+    const expense = parseInt(textAmount, 10);
+
+    // put it in object
+    expenseItem.desc = textDesc;
+    expenseItem.amount = expense;
+
+    allExpenses.push(expenseItem);
+    console.table(allExpenses);
+
+
+
+
+    // add that value to total expense
+    totalExpense = totalExpense + expense;
+
+    console.log({ totalExpense} );
+
+    // set heading element to total expnense
+    headingEl.textContent = totalExpense;
+
+
+}
+
+
+
+
+const element = document.querySelector("#btnAddExpense");
+element.addEventListener("click", addExpenseToTotal, false);
